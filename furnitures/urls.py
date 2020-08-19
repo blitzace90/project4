03897553a14d10e39700bbeab62ee178d28aadaf
blitzace90 <ongjunhaojames@gmail.com>
2020-08-19 02:57:34 +0000

    add hover effect

--- furnitures/urls.py
+++ furnitures/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 import furnitures.views
 
 urlpatterns = [
-    path('all', furnitures.views.show_furnitures, name='show_all_furnitures'),
+    path('all/', furnitures.views.show_furnitures, name='show_all_furnitures'),
     path('about', furnitures.views.about, name='aboutus'),
-    path('<furniture_id>', furnitures.views.furniture_details, name='furniture_details'),
+    path('<furniture_id>/', furnitures.views.furniture_details, name='furniture_details'),
     path('filter/<category_filter>', furnitures.views.filter_furnitures, name="filter"),
 ]
